f3130568c1869265d73994c1abd3c7346b3d75da
dazlx <iDontKnow>
2026-02-12 21:26:57 +0300

    Add start server func

--- cmd/solid-lamp/solid-lamp.go
+++ cmd/solid-lamp/solid-lamp.go
@@ -1,5 +1,9 @@
 package solidlamp
 
 import (
-	
+	Server "solid-lamp/internal/server"
 )
+
+func main() {
+	Server.Run()
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,15 @@
 package Server
+
+import (
+	"log"
+	"net/http"
+)
+
+func Run() {
+	log.Println("Server Started")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err == nil {
+		log.Println("Error:", err)
+	}
+}
