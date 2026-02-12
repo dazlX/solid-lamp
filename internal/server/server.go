d16d7bb374500972d868be0ae9feb33214254a7d
dazlx <iDontKnow>
2026-02-12 22:26:43 +0300

    Fix merge

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,14 +3,13 @@ package Server
 import (
 	"log"
 	"net/http"
-	handlers "solid-lamp/internal/handler"
+	"solid-lamp/internal/handler"
 )
 
 func Run() {
 	log.Println("Server Started")
-	http.HandleFunc("/", RootHandler)
 	http.HandleFunc("/api/health", handler.HealthHandler)
-	http.HandleFunc("/", handlers.RootHandler)
+	http.HandleFunc("/", handler.RootHandler)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err == nil {
